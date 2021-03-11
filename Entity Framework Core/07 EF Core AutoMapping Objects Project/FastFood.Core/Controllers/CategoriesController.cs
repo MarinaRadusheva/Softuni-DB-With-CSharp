c66cfd1f994c7eb234544ca35b9e7d8d9b805e0b
Marina <marinaradusheva9@gmail.com>
2021-03-11 23:55:17 +0200

    adding automapper exercise

---
+++ Entity Framework Core/07 EF Core AutoMapping Objects Project/FastFood.Core/Controllers/CategoriesController.cs
@@ -0,0 +1,47 @@
+﻿namespace FastFood.Core.Controllers
+{
+    using System;
+    using System.Linq;
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Data;
+    using FastFood.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using ViewModels.Categories;
+
+    public class CategoriesController : Controller
+    {
+        private readonly FastFoodContext context;
+        private readonly IMapper mapper;
+
+        public CategoriesController(FastFoodContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateCategoryInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            var category = mapper.Map<Category>(model);
+            context.Categories.Add(category);
+            context.SaveChanges();
+            return this.RedirectToAction("All", "Categories");
+        }
+
+        public IActionResult All()
+        {
+            var categories = context.Categories.ProjectTo<CategoryAllViewModel>(mapper.ConfigurationProvider).ToList();
+            return this.View(categories);
+        }
+    }
+}
---
+++ Entity Framework Core/07 EF Core AutoMapping Objects Project/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
@@ -0,0 +1,46 @@
+﻿namespace FastFood.Core.MappingConfiguration
+{
+    using AutoMapper;
+    using ViewModels.Employees;
+    using FastFood.Models;
+    using ViewModels.Positions;
+    using FastFood.Core.ViewModels.Categories;
+    using FastFood.Core.ViewModels.Items;
+    using FastFood.Core.ViewModels.Orders;
+    using System;
+
+    public class FastFoodProfile : Profile
+    {
+        public FastFoodProfile()
+        {
+            //Positions
+            this.CreateMap<CreatePositionInputModel, Position>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
+
+            this.CreateMap<Position, PositionsAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+            //Employees
+            this.CreateMap<Position, RegisterEmployeeViewModel>()
+                .ForMember(x => x.PositionId, y => y.MapFrom(s => s.Id));
+            this.CreateMap<RegisterEmployeeInputModel, Employee>();
+            this.CreateMap<Employee, EmployeesAllViewModel>()
+                .ForMember(x=>x.Position, y=>y.MapFrom(s=>s.Position.Name));
+            //Categories
+            this.CreateMap<CreateCategoryInputModel, Category>()
+                .ForMember(x=>x.Name, y=>y.MapFrom(s=>s.CategoryName));
+            this.CreateMap<Category, CategoryAllViewModel>();
+            this.CreateMap<Category, CreateItemViewModel>()
+                .ForMember(x=>x.CategoryId, y=>y.MapFrom(s=>s.Id));
+            //Items
+            this.CreateMap<CreateItemInputModel, Item>();
+            this.CreateMap<Item, ItemsAllViewModels>()
+                .ForMember(x=>x.Category, y=>y.MapFrom(x=>x.Category.Name));
+            //Orders
+            this.CreateMap<CreateOrderInputModel, Order>().ForMember(x => x.DateTime, y => y.MapFrom(s => DateTime.Now));
+            this.CreateMap<Order, OrderAllViewModel>()
+                .ForMember(x => x.Employee, y => y.MapFrom(s => s.Employee.Name))
+                .ForMember(x=>x.OrderId, y=>y.MapFrom(s=>s.Id))
+                .ForMember(x=>x.DateTime, y=>y.MapFrom(s=>s.DateTime.ToString("d")));
+        }
+    }
+}
